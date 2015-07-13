7289be3898976659df0980ce88ede52ac20221f8
Gigabyte-Giant <brynden.bielefeld@hotmail.com>
2015-07-13 13:07:56 -0500

    Updated project wrapper

--- Backend/contest_judging_sys.js
+++ Backend/contest_judging_sys.js
@@ -1,22 +1,25 @@
 /***
  * This file is where all the general purpose, reusable code should go!
 ***/
+var includeFunc = function(path) {
+	var scriptTag = document.createElement("script");
+	scriptTag.src = path;
+
+	document.body.appendChild(scriptTag);
+
+	return scriptTag;
+};
 
 window.Contest_Judging_System = (function() {
 	/* jQuery and Firebase are both dependencies for this project. If we don't have them, exit the function immediately. */
-	if (!jQuery || !Firebase) return;
+	if (!jQuery || !Firebase || !window.KA_API) return; // TODO: If a project dependency doesn't exist, go ahead an inject it.
 
 	return {
-		include: function(path) {
-			var scriptTag = document.createElement("script");
-			scriptTag.src = link;
-
-			document.body.appendChild(scriptTag);
-
-			return scriptTag;
-		},
+		include: includeFunc,
 		sync: function() {
 			// TODO
+			KA_API.getContests(function() {});
+			console.log("Sync function coming soon!");
 		}
-	}
+	};
 })();
