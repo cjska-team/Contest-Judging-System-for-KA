c900a393b88c934c8de37ff14188104e5b73fda1
Noble-Mushtak <Noble-Mushtak@users.noreply.github.com>
2015-07-13 11:18:58 -0400

    Gave aikaikaik credit for getApi()

--- Backend/contest_judging_sys.js
+++ Backend/contest_judging_sys.js
@@ -13,11 +13,13 @@ var include = function(link) {
     document.body.appendChild(scriptTag);
 };
 
-var getApi = function(apiUrl,complete){
-  return $.ajax({
-      type: 'GET',
-      url: apiUrl,
-      async: true,
-      complete: complete
+var getApi = function(apiUrl, complete){
+    /*Send a GET AJAX request to apiUrl and call complete when finished*/
+    //Made by aikaikaik in https://github.com/aikaikaik/KA-Contest-Judging-System/commit/dee9bceaef6c6fc8abf8437d76882e5cfca81718
+    return $.ajax({
+        type: 'GET',
+        url: apiUrl,
+        async: true,
+        complete: complete
     });
 };
