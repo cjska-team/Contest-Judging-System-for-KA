6002dc5fef7f6fd0b45785134512d4e38c2e7d00
Gigabyte-Giant <brynden.bielefeld@hotmail.com>
2015-07-14 09:07:56 -0500

    Updated some things in the backend

--- Backend/ka_api.js
+++ Backend/ka_api.js
@@ -13,50 +13,6 @@ window.KA_API = (function() {
 				return "https://www.khanacademy.org/api/internal/scratchpads/{PROGRAM}/top-forks?casing=camel&sort=2&limit=300000&page=0&lang=en".replace("{PROGRAM}", programID);
 			}
 		},
-		getContests: function(callback) {
-			/* Let's not return anything until we're done! */
-			var done = false;
-			/* Any contests that we find, will be put into this array. (We'll also return this array.) */
-			var contests = [];
-
-			var apiQuery = $.ajax({
-				type: 'GET',
-				url: this.urls.spotlight,
-				async: true,
-				complete: function(apiResponse) {
-					/* Instead of having to type apiResponse.responseJSON all the time, let's create a variable to hold the response json. */
-					var jsonData = apiResponse.responseJSON;
-
-					/* Keep track of all the spotlight programs */
-					var allPrograms = jsonData.scratchpads;
-
-					for (var i = 0; i < allPrograms.length; i++) {
-						var currentScratchpad = allPrograms[i];
-
-						/* For now, let's only accept contests from pamela */
-						if (currentScratchpad.authorNickname.match("pamela") !== null) {
-							/* Contest programs always have "Contest" in the title... */
-							if (currentScratchpad.translatedTitle.match("Contest") !== null) {
-								contests[currentScratchpad.url.split("/")[5]] = {
-									id: currentScratchpad.url.split("/")[5],
-									name: currentScratchpad.translatedTitle,
-									entries: [ /* TODO! */ ]
-								};
-							}
-						}
-					}
-					done = true;
-				}
-			});
-
-			/* Check to see if we're done; if we are, invoke the callback function. (Idea from @noble-mushtak) */
-			var finishTimeout = setInterval(function() {
-				if (done) {
-					clearInterval(finishTimeout);
-					callback(contests);
-				}
-			}, 1000);
-		},
 		getContestEntries: function(contestID, callback) {
 			/* Let's not return anything until we're done! */
 			var done = false;
@@ -89,6 +45,72 @@ window.KA_API = (function() {
 					callback(entries);
 				}
 			});
+		},
+		getContests: function(callback) {
+			/* Let's not return anything until we're done! */
+			var apiQueryDone = false;
+			/* Any contests that we find, will be put into this array. (We'll also return this array.) */
+			var contests = {};
+
+			var apiQuery = $.ajax({
+				type: 'GET',
+				url: this.urls.spotlight,
+				async: true,
+				complete: function(apiResponse) {
+					/* Instead of having to type apiResponse.responseJSON all the time, let's create a variable to hold the response json. */
+					var jsonData = apiResponse.responseJSON;
+
+					/* Keep track of all the spotlight programs */
+					var allPrograms = jsonData.scratchpads;
+					
+					for (var i = 0; i < allPrograms.length; i++) {
+						var loadedEntries = false;
+						var currentScratchpad = allPrograms[i];
+
+						var allEntries = {};
+
+						/* For now, let's only accept contests from pamela */
+						if (currentScratchpad.authorNickname.match("pamela") !== null) {
+							/* Contest programs always have "Contest" in the title... */
+							if (currentScratchpad.translatedTitle.match("Contest") !== null) {
+								var programID = currentScratchpad.url.split("/")[5];
+
+								contests[programID] = {
+									id: programID,
+									name: currentScratchpad.translatedTitle
+								};
+
+								(function() {
+									var programIDinside = programID;
+
+									KA_API.getContestEntries(contests[programIDinside].id, function(entries) {
+										contests[programIDinside].entries = entries;
+									});
+								})();
+							}
+						}
+					}
+					apiQueryDone = true;
+				}
+			});
+
+			/* Check to see if we're done; if we are, invoke the callback function. (Idea from @noble-mushtak) */
+			var finishTimeout = setInterval(function() {
+				var done = apiQueryDone;
+
+				if (done) {
+					for (var i in contests) {
+						if (contests[i].entries === undefined) {
+							done = false;
+							return;
+						}
+					}
+
+					console.log("All done!");
+					callback(contests);
+					clearInterval(finishTimeout);
+				}
+			}, 1000);
 		}
 	};
 })();
