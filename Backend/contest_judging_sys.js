d86d0c3240b8de321e2252f72014d7ab8e1ea457
Noble-Mushtak <Noble-Mushtak@users.noreply.github.com>
2015-07-15 10:56:02 -0400

    Create upstream repo with new Client

---
+++ Backend/contest_judging_sys.js
@@ -0,0 +1,82 @@
+/***
+ * This file is where all the general purpose, reusable code should go!
+***/
+
+/* This function can be used to inject scripts into pages. */
+var includeFunc = function(path) {
+	var scriptTag = document.createElement("script");
+	scriptTag.src = path;
+
+	document.body.appendChild(scriptTag);
+
+	return scriptTag;
+};
+
+window.Contest_Judging_System = (function() {
+	/* jQuery and Firebase are both dependencies for this project. If we don't have them, exit the function immediately. */
+	if (!jQuery || !Firebase || !window.KA_API) return; // TODO: If a project dependency doesn't exist, go ahead an inject it.
+
+	/* Everything from this namespace will be placed in the object that is returned. */
+	return {
+		/* Puts the script injection function inside of this namespace. */
+		include: includeFunc,
+		/* Get's all the contests that we have stored on Khan Academy, and passes them into a callback function. */
+		getStoredContests: function(callback) {
+			var fbRef = new Firebase("https://contest-judging-sys.firebaseio.com/contests/");
+
+			/* An array to hold all the data from Firebase. */
+			var fromFirebase = {};
+
+			fbRef.orderByKey().on("child_added", function(item) {
+				fromFirebase[item.key()] = item.val();
+			});
+			fbRef.once("value", function(data) {
+				callback(fromFirebase);
+			});
+		},
+		sync: function(callback) {
+			/*
+			 * Sync just fetches the latest data from Khan Academy and Firebase, and compares it.
+			 * If the data does from each source does not match, we add anything that needs to be added, and delete anything that needs to be deleted (deleting has not been implemented yet).
+			 * We have two arrays of data; kaData and fbData.
+			 * We loop through every item in both, to determine what doesn't exist in the opposite source.
+			 * Originally authored by Gigabyte Giant
+			 * TODO: Perform added/deleted checking on contest entries.
+			 */
+			var completed = {
+				firebase: false,
+				khanacademy: false
+			};
+
+			/* Currently not used. Might be re-implemented in the future. */
+			var addedContests = [];
+			var removedContests = [];
+
+			var kaData;
+			var fbData;
+
+			/* Get all the contests from Khan Academy. */
+			KA_API.getContests(function(response) {
+				kaData = response;
+				completed.khanacademy = true;
+			});
+
+			/* Get all of the known contests from Firebase */
+			this.getStoredContests(function(response) {
+				fbData = response;
+				completed.firebase = true;
+			});
+
+			/* Create a new reference to Firebase, to use later on when pushing contests to Firebase. */
+			var fbRef = new Firebase("https://contest-judging-sys.firebaseio.com/contests/");
+
+			/* Wait until data has been recieved from Firebase and Khan Academy, then push new(er) data to Firebase. */
+			var recievedData = setInterval(function() {
+				if (completed.firebase && completed.khanacademy) {
+					clearInterval(recievedData);
+					fbRef.set(kaData);
+				}
+			}, 1000);
+		}
+	};
+})();
---
+++ Backend/ka_api.js
@@ -0,0 +1,122 @@
+/***
+ * This is a "wrapper" of sorts, for the Khan Academy API.
+ * Anything relating to the Khan Academy API should be placed in this file.
+***/
+window.KA_API = (function() {
+	/* jQuery is required for [most] all of these. So if it's object is not found, exit the function. */
+	if (!jQuery) return;
+
+	/* Everything from this namespace will be placed in the object that is returned. */
+	return {
+		/* Khan Academy API Urls */
+		urls: {
+			spotlight: "https://www.khanacademy.org/api/internal/scratchpads/top?casing=camel&topic_id=xffde7c31&sort=4&limit=40000&page=0&lang=en&_=1436581332879",
+			spinoffs: function(programID) {
+				return "https://www.khanacademy.org/api/internal/scratchpads/{PROGRAM}/top-forks?casing=camel&sort=2&limit=300000&page=0&lang=en".replace("{PROGRAM}", programID);
+			}
+		},
+		/* This function gets all the entries for a specific contest, and passes them into the callback. */
+		getContestEntries: function(contestID, callback) {
+			/* Let's not return anything until we're done! */
+			var done = false;
+			/* Any entries that we find, will be put into this object. (We'll also return this object.) */
+			var entries = {};
+
+			/* The ajax request used for getting all the entries to the desired contest. */
+			var apiQuery = $.ajax({
+				type: 'GET',
+				url: this.urls.spinoffs(contestID),
+				async: true,
+				complete: function(apiResponse) {
+					/* Instead of having to type apiResponse.responseJSON all the time, let's create a variable to hold the response json. */
+					var jsonData = apiResponse.responseJSON;
+
+					for (var i = 0; i < jsonData.scratchpads.length; i++) {
+						var id = jsonData.scratchpads[i].url.split("/")[5];
+
+						/* Put the current entries in the object that we're going to "return". */
+						entries[id] = {
+							id: id,
+							name: jsonData.scratchpads[i].translatedTitle,
+							scores: {
+								rubric: 0,
+								judge: 0
+							}
+						};
+					}
+					/* Pass all the entries we found, to the callback function. */
+					callback(entries);
+				}
+			});
+		},
+		/* Get's all of Pamela's contest programs from Khan Academy, and pass them into the callback function. */
+		getContests: function(callback) {
+			/* Let's not return anything until we're done! */
+			var apiQueryDone = false;
+			/* Any contests that we find, will be put into this array. (We'll also return this array.) */
+			var contests = {};
+
+			/* The ajax request used to get all the contests from Khan Academy. */
+			var apiQuery = $.ajax({
+				type: 'GET',
+				url: this.urls.spotlight,
+				async: true,
+				complete: function(apiResponse) {
+					/* Instead of having to type apiResponse.responseJSON all the time, let's create a variable to hold the response json. */
+					var jsonData = apiResponse.responseJSON;
+
+					/* Keep track of all the spotlight programs */
+					var allPrograms = jsonData.scratchpads;
+					
+					for (var i = 0; i < allPrograms.length; i++) {
+						var loadedEntries = false;
+						var currentScratchpad = allPrograms[i];
+
+						var allEntries = {};
+
+						/* For now, let's only accept contests from pamela */
+						if (currentScratchpad.authorNickname.match("pamela") !== null) {
+							/* Contest programs always have "Contest" in the title... */
+							if (currentScratchpad.translatedTitle.match("Contest") !== null) {
+								var programID = currentScratchpad.url.split("/")[5];
+
+								contests[programID] = {
+									id: programID,
+									name: currentScratchpad.translatedTitle,
+									img: currentScratchpad.thumb
+								};
+
+								(function() {
+									var programIDinside = programID;
+
+									KA_API.getContestEntries(contests[programIDinside].id, function(entries) {
+										contests[programIDinside].entries = entries;
+									});
+								})();
+							}
+						}
+					}
+					apiQueryDone = true;
+				}
+			});
+
+			/* Check to see if we're done; if we are, invoke the callback function. (Idea from @noble-mushtak) */
+			var finishTimeout = setInterval(function() {
+				var done = apiQueryDone;
+
+				if (done) {
+					for (var i in contests) {
+						if (contests[i].entries === undefined) {
+							done = false;
+							return;
+						}
+					}
+
+					console.log("All done!");
+					callback(contests);
+					clearInterval(finishTimeout);
+				}
+			}, 1000);
+		}
+	};
+})();
