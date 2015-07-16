81c31aaaffd9ef22b81e1fcecb90086d6b762282
Noble-Mushtak <Noble-Mushtak@users.noreply.github.com>
2015-07-15 21:17:01 -0400

    Created important branch

--- Backend/contest_judging_sys.js
+++ Backend/contest_judging_sys.js
@@ -1,82 +1,88 @@
 /***
  * This file is where all the general purpose, reusable code should go!
 ***/
-
-/* This function can be used to inject scripts into pages. */
-var includeFunc = function(path) {
-	var scriptTag = document.createElement("script");
-	scriptTag.src = path;
-
-	document.body.appendChild(scriptTag);
-
-	return scriptTag;
+/* This function can be used to inject scripts into pages. It returns the created <script> element. */
+//The reason we make it a global function even though it's in the below namespace is because outside JavaScript files might need to use this function after this function loads, but before the namespace is loaded.
+window.includeFunc = function(path) {
+    var scriptTag = document.createElement("script");
+    scriptTag.src = path;
+    document.body.appendChild(scriptTag);
+    return scriptTag;
 };
 
 window.Contest_Judging_System = (function() {
-	/* jQuery and Firebase are both dependencies for this project. If we don't have them, exit the function immediately. */
-	if (!jQuery || !Firebase || !window.KA_API) return; // TODO: If a project dependency doesn't exist, go ahead an inject it.
+    /* Function wrapper to create Contest_Judging_System */
+    
+    //jQuery and Firebase are both dependencies for this project. If we don't have them, exit the function immediately.
+    // TODO: If a project dependency doesn't exist, go ahead an inject it.
+    if (!window.jQuery || !window.Firebase || !window.KA_API) {
+        console.log("Needs jQuery, Firebase, and KA_API");
+        return;
+    }
 
-	/* Everything from this namespace will be placed in the object that is returned. */
-	return {
-		/* Puts the script injection function inside of this namespace. */
-		include: includeFunc,
-		/* Get's all the contests that we have stored on Khan Academy, and passes them into a callback function. */
-		getStoredContests: function(callback) {
-			var fbRef = new Firebase("https://contest-judging-sys.firebaseio.com/contests/");
+    //Everything from this namespace will be placed in the object that is returned.
+    return {
+        /* Puts the script injection function inside of this namespace. */
+        include: includeFunc,
+        /* This function gets all the contests that we have stored on Firebase and passes them into a callback function. */
+        getStoredContests: function(callback) {
+            //This is the object for the contests within our Firebase database.
+            var fbRef = new Firebase("https://contest-judging-sys.firebaseio.com/contests/");
+            //This is an object to hold all of the data from Firebase.
+            var fromFirebase = {};
+            //Insert all of the entries in our database in order by key
+            fbRef.orderByKey().on("child_added", function(item) {
+                fromFirebase[item.key()] = item.val();
+            });
+            //Finally, pass fromFirebase into callback.
+            fbRef.once("value", function(data) {
+                callback(fromFirebase);
+            });
+        },
+        sync: function(callback) {
+            /*
+             * sync() just fetches the latest data from Khan Academy and Firebase, and compares it.
+             * We have two arrays of data; kaData and fbData. We get the data using the KA_API and the above getStoredContests() method.
+             * Once both requests have finished, we set fbData to kaData using the Firebase set() method.
+             * Originally authored by Gigabyte Giant
+             * TODO: Perform added/deleted checking on contest entries.
+             * TODO: Actually do something with callback
+             * TODO: Actually do something with fbData
+             */
+            //These two Booleans check whether or not both requests have been completed.
+            var completed = {
+                firebase: false,
+                khanacademy: false
+            };
+            /* Currently not used. Might be re-implemented in the future. */
+            //var addedContests = [];
+            //var removedContests = [];
+            //Our two arrays of data
+            var kaData;
+            var fbData;
 
-			/* An array to hold all the data from Firebase. */
-			var fromFirebase = {};
+            //Get all of the contests from Khan Academy
+            KA_API.getContests(function(response) {
+                /* When done, set kaData to the contests and set completed.khanacademy to true. */
+                kaData = response;
+                completed.khanacademy = true;
+            });
+            //Get all of the known contests from Firebase
+            this.getStoredContests(function(response) {
+                /* When done, set fbData to our stored contests and set completed.firebase to true. */
+                fbData = response;
+                completed.firebase = true;
+            });
 
-			fbRef.orderByKey().on("child_added", function(item) {
-				fromFirebase[item.key()] = item.val();
-			});
-			fbRef.once("value", function(data) {
-				callback(fromFirebase);
-			});
-		},
-		sync: function(callback) {
-			/*
-			 * Sync just fetches the latest data from Khan Academy and Firebase, and compares it.
-			 * If the data does from each source does not match, we add anything that needs to be added, and delete anything that needs to be deleted (deleting has not been implemented yet).
-			 * We have two arrays of data; kaData and fbData.
-			 * We loop through every item in both, to determine what doesn't exist in the opposite source.
-			 * Originally authored by Gigabyte Giant
-			 * TODO: Perform added/deleted checking on contest entries.
-			 */
-			var completed = {
-				firebase: false,
-				khanacademy: false
-			};
-
-			/* Currently not used. Might be re-implemented in the future. */
-			var addedContests = [];
-			var removedContests = [];
-
-			var kaData;
-			var fbData;
-
-			/* Get all the contests from Khan Academy. */
-			KA_API.getContests(function(response) {
-				kaData = response;
-				completed.khanacademy = true;
-			});
-
-			/* Get all of the known contests from Firebase */
-			this.getStoredContests(function(response) {
-				fbData = response;
-				completed.firebase = true;
-			});
-
-			/* Create a new reference to Firebase, to use later on when pushing contests to Firebase. */
-			var fbRef = new Firebase("https://contest-judging-sys.firebaseio.com/contests/");
-
-			/* Wait until data has been recieved from Firebase and Khan Academy, then push new(er) data to Firebase. */
-			var recievedData = setInterval(function() {
-				if (completed.firebase && completed.khanacademy) {
-					clearInterval(recievedData);
-					fbRef.set(kaData);
-				}
-			}, 1000);
-		}
-	};
+            //Create a new reference to Firebase to use later on when pushing contests to Firebase.
+            var fbRef = new Firebase("https://contest-judging-sys.firebaseio.com/contests/");
+            //Every second, we check if both requests have been completed and if they have, we stop checking if both requests have been completed and set fbRef to kaData using the Firebase set() method.
+            var recievedData = setInterval(function() {
+                if (completed.firebase && completed.khanacademy) {
+                    clearInterval(recievedData);
+                    fbRef.set(kaData);
+                }
+            }, 1000);
+        }
+    };
 })();
