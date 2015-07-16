ee23ce174036f699488950d832f930c2244691c6
Gigabyte-Giant <brynden.bielefeld@hotmail.com>
2015-07-16 16:24:33 -0500

    Made the contest entry viewer page thing look a bit nicer

--- Client/scripts/contest.js
+++ Client/scripts/contest.js
@@ -13,7 +13,7 @@ $(function() {
 	var contestId = window.location.href.split("?contest=")[1];
 
 	/* Go ahead and find the div that we'll store all the entries in. */
-	var entriesDiv = document.querySelector("#entries");
+	var entriesList = document.querySelector(".media-list");
 
 	console.log("Contest found!");
 	console.log("Contest ID: " + contestId);
@@ -34,29 +34,50 @@ $(function() {
 
 			/* Add all entries to the page */
 			for (var i in entries) {
-				/* Instead of having to write entries[i] all the time, let's declare a variable that's a bit shorter. */
-				var curr = entries[i];
+				(function() {
+					/* Instead of having to write entries[i] all the time, let's declare a variable that's a bit shorter. */
+					var curr = entries[i];
 
-				/* Create a div and give it Bootstrap's "row" class */
-				var rowDiv = document.createElement("row");
-				rowDiv.className = "row";
+					$.ajax({
+						type: 'GET',
+						url: "https://www.khanacademy.org/api/labs/scratchpads/" + curr.id,
+						async: true,
+						complete: function(response) {
+							/* Create a list item element, and give it Bootstrap's "media" class. */
+							var mediaListItem = document.createElement("li");
+							mediaListItem.className = "media entry";
 
-				/* Create a paragraph element that'll eventually hold a link to this entry */
-				var paragraphElem = document.createElement("p");
+							/* Create a div element, and give it Bootstrap's "media-left" class. */
+							var mediaLeftDiv = document.createElement("div");
+							mediaLeftDiv.className = "media-left";
 
-				/* Create a link element and set it's text to the name of this entry */
-				var aElem = document.createElement("a");
-				aElem.textContent = curr.name;
-				aElem.href = "https://www.khanacademy.org/computer-programming/entry/" + curr.id;
+							/* Create a link element, and set it's href to the URL of this entry... */
+							var aElem = document.createElement("a");
+							aElem.href = "https://www.khanacademy.org/computer-programming/entry" + curr.id;
 
-				/* Add the link element to our paragraph element */
-				paragraphElem.appendChild(aElem);
+							/* Create an image element and set it's src to this entry's thumbnail */
+							var mediaObj = document.createElement("img");
+							mediaObj.src = response.responseJSON.imageUrl;
+							mediaObj.alt = "Entry thumbnail";
 
-				/* Add our paragraph element to our row div */
-				rowDiv.appendChild(paragraphElem);
+							/* Create a div element, and give it Bootstrap's "media-body" class. */
+							var mediaBody = document.createElement("div");
+							mediaBody.className = "media-body";
 
-				/* Add our row div to the entries div */
-				entriesDiv.appendChild(rowDiv);
+							/* Create a heading element (tier 4), and set it's text to this entry's name. */
+							var mediaHeading = document.createElement("h4");
+							mediaHeading.textContent = curr.name;
+
+							/* Append everything */
+							aElem.appendChild(mediaObj);
+							mediaLeftDiv.appendChild(aElem);
+							mediaBody.appendChild(mediaHeading);
+							mediaListItem.appendChild(mediaLeftDiv);
+							mediaListItem.appendChild(mediaBody);
+							entriesList.appendChild(mediaListItem);
+						}
+					});
+				})();
 			}
 
 			/* Hide the loading div and show items that were hidden during loading. */
