528c21dbeb19def1e3290716c36d5429db56aaaa
Gigabyte-Giant <brynden.bielefeld@hotmail.com>
2015-07-22 13:37:06 -0500

    Removed useless function

--- Backend/contest_judging_sys.js
+++ Backend/contest_judging_sys.js
@@ -45,19 +45,6 @@ window.Contest_Judging_System = (function() {
                 callback(rubrics);
             });
         },
-        rubricValToKey: function(rubricName, rubricVal, callback) {
-            Contest_Judging_System.getRubrics(function(rubrics) {
-                var currRubric = rubrics[rubricName];
-
-                if (currRubric) {
-                    if (currRubric.hasOwnProperty("keys")) {
-                        callback(currRubric.keys[rubricVal]);
-                    }
-                }
-
-                callback(undefined);
-            });
-        },
         /* This function gets all the contests that we have stored on Firebase and passes them into a callback function. */
         getStoredContests: function(callback) {
             /* This is the object for the contests within our Firebase database. */
@@ -221,7 +208,7 @@ window.Contest_Judging_System = (function() {
                             }
                         }
                     }
-                    console.log(entriesToAdd);
+
                     /* Loop through all the data we recieved from Firebase; and see if it still exists on Khan Academy. */
                     for (var i in fbData) {
                         if (!kaData.hasOwnProperty(i)) {
