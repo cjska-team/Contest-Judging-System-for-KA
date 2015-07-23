2098d55b5ed0768a5d284017298bbebc4d1d9ec6
Gigabyte-Giant <brynden.bielefeld@hotmail.com>
2015-07-23 17:11:38 -0500

    Removed Authentication_Logic from the window object

--- Client/scripts/auth/auth.js
+++ Client/scripts/auth/auth.js
@@ -1,7 +1,7 @@
 /*
  * This file contains all the authentication logic for the Khan Academy Contest Judging System
  */
-window.Authentication_Logic = (function() {
+var Authentication_Logic = (function() {
 	if (!Firebase || !jQuery || !Contest_Judging_System) {
 		console.log("[Authentication_Logic] Firebase and jQuery are both required!");
 		return;
