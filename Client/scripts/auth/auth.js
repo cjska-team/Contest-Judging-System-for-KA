5614f9163b6dcdaeb96f0cfe4761fc547c3fb3bb
Gigabyte-Giant <brynden.bielefeld@hotmail.com>
2015-07-23 16:48:10 -0500

    More work on authjs

--- Client/scripts/auth/auth.js
+++ Client/scripts/auth/auth.js
@@ -8,6 +8,41 @@ window.Authentication_Logic = (function() {
 	}
 
 	return {
-		
+		doesUserExist: function(authData, callback) {
+			var fbRef = new Firebase("https://contest-judging-sys.firebaseio.com");
+			var usersRef = fbRef.child("users");
+
+			var knownUsers = { };
+
+			usersRef.orderByKey().on("child_added", function(snapshot) {
+				knownUsers[snapshot.key()] = snapshot.val();
+			});
+
+			usersRef.once("value", function() {
+				/* If this user's UID was found in knownUsers... */
+				if (knownUsers.hasOwnProperty(authData.uid)) {
+					/* ...pass true to our callback! */
+					callback(true);
+				} else {
+					/* ...pass false into our callback. */
+					callback(true);
+				}
+			});
+		},
+		userHasCorrectPermissions: function(uid, requiredPerms, callback) {
+			var fbRef = new Firebase("https://contest-judging-sys.firebaseio.com");
+			var usersRef = fbRef.child("users");
+
+			var thisUser = { };
+
+			usersRef.orderByKey().equalTo(uid).on("child_added", function(snapshot) {
+				thisUser = snapshot.val();
+			});
+
+			usersRef.once("value", function() {
+				/* Pass a boolean value to our callback. The boolean is determined based on whether or not the user has the permission level that is required for a certain task. */
+				callback(thisUser.permLevel === requiredPerms);
+			});
+		}
 	};
 })();
