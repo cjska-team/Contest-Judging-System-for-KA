edc9a1d6793b2773b367881fe455bf4ea129f314
Gigabyte-Giant <brynden.bielefeld@hotmail.com>
2015-07-23 16:33:53 -0500

    New wrapper for authentication

--- Client/scripts/auth/auth.js
+++ Client/scripts/auth/auth.js
@@ -1,3 +1,13 @@
 /*
  * This file contains all the authentication logic for the Khan Academy Contest Judging System
  */
+window.Authentication_Logic = (function() {
+	if (!Firebase || !jQuery) {
+		console.log("[Authentication_Logic] Firebase and jQuery are both required!");
+		return;
+	}
+
+	return {
+		
+	};
+})();
