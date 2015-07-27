e5bbc4cec73ae8c351a402fdf2fa2f127d3173c4
Gigabyte-Giant <brynden.bielefeld@hotmail.com>
2015-07-27 11:35:23 -0500

    Sketching things out

--- Client/admin/scripts/main.js
+++ Client/admin/scripts/main.js
@@ -4,3 +4,17 @@
  * Step 3: Load all tools onto the page.
  * Step 4: [ ... ]
  */
+
+/* Check if user is logged in. We'll be using cookies to store whether or not the user is logged in (unfortunately). */
+if (Contest_Judging_System.getCookie("isUserLoggedIn") === "yes" && Contest_Judging_System.getCookie("loggedInUser") !== "") {
+	/* User must be logged in! */
+	var fbRef = new Firebase("https://contest-judging-sys.firebaseio.com/users/");
+
+	var userID = Contest_Judging_System.getCookie("loggedInUser");
+
+	/* Check to make sure the logged in user is an admin. */
+	/* TODO */
+} else {
+	/* User must not be logged in... ;( */
+	/* TODO */
+}
