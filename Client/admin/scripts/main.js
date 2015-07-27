ad11b1440b6d2cb61958106ec7d9f37a5cafee9e
Gigabyte-Giant <brynden.bielefeld@hotmail.com>
2015-07-27 11:48:10 -0500

    Some more work on admin/main.js

--- Client/admin/scripts/main.js
+++ Client/admin/scripts/main.js
@@ -6,14 +6,32 @@
  */
 
 /* Check if user is logged in. We'll be using cookies to store whether or not the user is logged in (unfortunately). */
-if (Contest_Judging_System.getCookie("isUserLoggedIn") === "yes" && Contest_Judging_System.getCookie("loggedInUser") !== "") {
+if (Contest_Judging_System.getCookie("loggedInUser") !== "") {
 	/* User must be logged in! */
 	var fbRef = new Firebase("https://contest-judging-sys.firebaseio.com/users/");
 
 	var userID = Contest_Judging_System.getCookie("loggedInUser");
 
-	/* Check to make sure the logged in user is an admin. */
-	/* TODO */
+	/* Query Firebase */
+	fbRef.orderByKey().on("child_added", function(data) { /* Do nothing here. */ });
+
+	/* Once the query is done... */
+	fbRef.once("value", function(data) {
+		/* ...make sure the user exists in Firebase. If they do... */
+		if (data.val().hasOwnProperty(userID)) {
+			/* ...make sure they're an admin. */
+			if (data.val()[userID].permLevel === 5) {
+				/* User appears to be an administrator! */
+				console.log(data.val()[userID].name + " is an admin!");
+			} else {
+				/* User doesn't appear to be an admin. */
+				console.log(data.val()[userID].name + " is not an admin!");
+			}
+		} else {
+			/* User doesn't exist in Firebase, which means they cannot be an admin. */
+			console.log("User ID \"" + userID + "\" doesn't exist in Firebase.");
+		}
+	});
 } else {
 	/* User must not be logged in... ;( */
 	/* TODO */
