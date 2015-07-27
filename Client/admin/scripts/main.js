ee85faf4f0ac1be06a27053feb6bdebf76c43317
Gigabyte-Giant <brynden.bielefeld@hotmail.com>
2015-07-27 11:57:08 -0500

    Added redirects

--- Client/admin/scripts/main.js
+++ Client/admin/scripts/main.js
@@ -7,9 +7,9 @@
 
 /* Check if user is logged in. We'll be using cookies to store whether or not the user is logged in (unfortunately). */
 if (Contest_Judging_System.getCookie("loggedInUser") === "") {
-	console.log("User doesn't appear to be logged in.");
+	alert("You don't appear to be logged in! Leaving page.");
+	window.location.assign("../index.html");
 } else {
-	/* User must be logged in! */
 	var fbRef = new Firebase("https://contest-judging-sys.firebaseio.com/users/");
 
 	var userID = Contest_Judging_System.getCookie("loggedInUser");
@@ -22,12 +22,14 @@ if (Contest_Judging_System.getCookie("loggedInUser") === "") {
 		/* ...make sure the user exists in Firebase. If they do... */
 		if (!data.val().hasOwnProperty(userID)) {
 			/* User doesn't exist in Firebase, which means they cannot be an admin. */
-			console.log("User ID \"" + userID + "\" doesn't exist in Firebase.");
+			alert("User ID \"" + userID + "\" doesn't exist in Firebase. Leaving page.");
+			window.location.assign("../index.html");
 		} else {
 			/* ...make sure they're an admin. */
 			if (data.val()[userID].permLevel !== 5) {
 				/* User doesn't appear to be an admin. */
-				console.log(data.val()[userID].name + " is not an admin!");
+				alert("You do not have admin permissions! Leaving page.");
+				window.location.assign("../index.html");
 			}
 		}
 	});
