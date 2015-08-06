9d99d9069bfdeac1d129d23227d92518b5131e58
Noble-Mushtak <Noble-Mushtak@users.noreply.github.com>
2015-08-05 20:30:30 -0400

    Improved auth/auth.js

--- Client/scripts/auth/auth.js
+++ Client/scripts/auth/auth.js
@@ -11,15 +11,34 @@ if (fbAuth == null) {
 	console.log("You're logged in! Yay!");
 }
 
+/* When the user clicks the login button... */
 loginButton.on("click", function() {
+    /* Connect to Firebase: */
 	var fbRef = new Firebase("https://contest-judging-sys.firebaseio.com");
 
+    /* Try to log the user in: */
 	fbRef.authWithOAuthPopup("google", function(error, authData) {
+        /* Error Handling: */
 		if (error) {
 			alert("An error occured. Please try again later.");
 			console.log(error);
 		} else {
-			loginButton.css("display", "none");
+            /* Update fbAuth */
+            fbAuth = Contest_Judging_System.getFirebaseAuth();
+            /* Add this user as someone with no permissions in Firebase if they don't already exist. */
+            var usersRef = fbRef.child("users");
+            usersRef.on("value", function(snapshot) { 
+                if (!snapshot.hasChild(fbAuth.uid)) {
+                    usersRef.child(fbAuth.uid).set({
+                        name: fbAuth.google.displayName,
+                        permLevel: 1
+                    });
+                    console.log("Added new user in Firebase!");
+                }
+                console.log("Logged user in!");
+                /* When we're done, hide the login button. */
+	     		loginButton.css("display", "none");
+            });
 		}
         /* This makes Firebase remember the login for 30 days (which has been set as the default in Firebase). */
 	}, {remember: "default"});
