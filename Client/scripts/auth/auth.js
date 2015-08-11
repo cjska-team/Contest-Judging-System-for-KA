7c7188069cd610cf7452b1c080976166f8fecb56
Gigabyte-Giant <brynden.bielefeld@hotmail.com>
2015-08-11 12:47:09 -0500

    Added logout button

--- Client/scripts/auth/auth.js
+++ Client/scripts/auth/auth.js
@@ -3,12 +3,14 @@
  */
 /* The login button: */
 var loginButton = $(".login");
+var logoutButton = $(".logout");
 /* Check if the user is logged in and make sure the login button is available if they're not. */
 var fbAuth = Contest_Judging_System.getFirebaseAuth();
 if (fbAuth == null) {
 	loginButton.css("display", "block");
 } else {
 	console.log("You're logged in! Yay!");
+	logoutButton.css("display", "block");
 	document.querySelector(".hideWhileNotAuthed").style.display = "block";
 }
 
@@ -17,6 +19,15 @@ loginButton.on("click", function() {
     Contest_Judging_System.logUserIn(function(authData) {
         /* Hide login button when done: */
         loginButton.css("display", "none");
+        logoutButton.css("display", "block");
 		document.querySelector(".hideWhileNotAuthed").style.display = "block";
     });
+});
+
+logoutButton.on("click", function() {
+	var fbRef = new Firebase("https://contest-judging-sys.firebaseio.com/");
+	fbRef.unauth();
+
+	logoutButton.css("display", "none");
+	loginButton.css("display", "block");
 });
