c742eb5ecd59f9045a68b44dfd9821517639eeba
Gigabyte-Giant <brynden.bielefeld@hotmail.com>
2015-08-12 09:14:59 -0500

    Got rid of Extension code (for now)

--- Extension/scripts/content.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	/*var allowNotificationUpdates = true;
-	chrome.storage.sync.get("allowNU", function(data){
-		allowNotificationUpdates = data; //Must Fix
-	});*/
-	$.getScript(chrome.extension.getURL("scripts/inject/notifications.js"));
-});
