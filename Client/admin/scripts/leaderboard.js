07b0cefd117cb735d0eb0420fcbb5cb67780e1ce
Gigabyte-Giant <brynden.bielefeld@hotmail.com>
2015-09-03 16:15:12 -0500

    Whitespace correcctions and more unique variable names in loops

--- Client/admin/scripts/leaderboard.js
+++ Client/admin/scripts/leaderboard.js
@@ -53,27 +53,27 @@ function loadLeaderboard(contestData, entryData) {
         /* The list of all the table data: */
         var tableDataList = [ ];
         /* For all entries: */
-        for (var k in entryData) {
+        for (var eD in entryData) {
             /* This is the object representing the table data for this entry: */
             var objToAdd = {
-                "Entry ID": entryData[k].id,
-                "Entry Title": "<a href=\"https://www.khanacademy.org/computer-programming/entry/"+entryData[k].id+"\">"+entryData[k].name+"</a>"
+                "Entry ID": entryData[eD].id,
+                "Entry Title": "<a href=\"https://www.khanacademy.org/computer-programming/entry/" + entryData[eD].id + "\">" + entryData[eD].name + "</a>"
             };
 
             /* The summed score for this entry: */
             var summedScore = 0;
             /* For each rubric: */
-            for (var i = 0; i < rubrics.Order.length; i++) {
+            for (var roInd = 0; i < rubrics.Order.length; roInd++) {
                 /* The score for this rubric is the average, unless it hasn't been graded yet, in which case it's the minimum: */
-                var score = entryData[k].scores.rubric.hasOwnProperty(rubrics.Order[i]) ? entryData[k].scores.rubric[rubrics.Order[i]].avg : rubrics[rubrics.Order[i]].min;
+                var score = entryData[eD].scores.rubric.hasOwnProperty(rubrics.Order[roInd]) ? entryData[eD].scores.rubric[rubrics.Order[roInd]].avg : rubrics[rubrics.Order[roInd]].min;
                 /* Increment summedScore by score: */
                 summedScore += score;
                 /* If this is a keys rubric, set the scores using score and the keys property: */
-                if (rubrics[rubrics.Order[i]].hasOwnProperty("keys")) {
-                    objToAdd[rubrics.Order[i].replace(/_/gi, " ")] = "("+score+") "+rubrics[rubrics.Order[i]].keys[score];
+                if (rubrics[rubrics.Order[roInd]].hasOwnProperty("keys")) {
+                    objToAdd[rubrics.Order[roInd].replace(/_/gi, " ")] = "(" + score + ") " + rubrics[rubrics.Order[roInd]].keys[score];
                 }
                 else {
-                    objToAdd[rubrics.Order[i].replace(/_/gi, " ")] = score;
+                    objToAdd[rubrics.Order[roInd].replace(/_/gi, " ")] = score;
                 }
             }
             /* Add summedScore to objToAdd: */
@@ -90,16 +90,19 @@ function loadLeaderboard(contestData, entryData) {
 
         /* Get the hashtag of the link (if there is one): */
         var hashtagIndex = window.location.href.indexOf("#");
+
         if (hashtagIndex != -1) {
-            var hashtagLink = window.location.href.substring(hashtagIndex+1, window.location.href.length);
+            var hashtagLink = window.location.href.substring(hashtagIndex + 1, window.location.href.length);
+
             /* Make sure that the hashtagLink starts with "SortingOrder:": */
             var string = "SortingOrder:";
+
             if (hashtagLink.substring(0, string.length) == string) {
                 /* Get the info from our hashtag link about the ordering: */
                 var columnIndex = parseInt(hashtagLink.substring(string.length, hashtagLink.length));
-                var lastLetter = hashtagLink.substring(hashtagLink.length-1, hashtagLink.length);
+                var lastLetter = hashtagLink.substring(hashtagLink.length - 1, hashtagLink.length);
                 /* If our information is valid, sort the DataTable based off of it: */
-                if (!isNaN(columnIndex) && columnIndex >= 0 && columnIndex < rubrics.Order.length+3 && (lastLetter == "A" || lastLetter == "D")) {
+                if (!isNaN(columnIndex) && columnIndex >= 0 && columnIndex < rubrics.Order.length + 3 && (lastLetter == "A" || lastLetter == "D")) {
                     leaderboardObj.order([columnIndex, (lastLetter == "A") ? "asc" : "desc"]).draw();
                 }
             }
@@ -108,7 +111,7 @@ function loadLeaderboard(contestData, entryData) {
         var sortingArrows = $("#leaderboardTable td[aria-controls]");
         sortingArrows.on("click", function() {
             /* Change the hashtag link based off the column sorted and whether or not it was ascending or descending: */
-            window.location.href = "#SortingOrder:"+sortingArrows.index(this)+(this.className.indexOf("sorting_desc") == -1 ? "A" : "D");
+            window.location.href = "#SortingOrder:" + sortingArrows.index(this) + (this.className.indexOf("sorting_desc") == -1 ? "A" : "D");
         });
 
         /* Hide the loading message and show the content: */
