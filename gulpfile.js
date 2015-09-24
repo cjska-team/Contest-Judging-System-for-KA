1bec620ef77f05bb28f73b1c45bb74a777646086
Gigabyte-Giant <brynden.bielefeld@hotmail.com>
2015-09-24 10:59:53 -0500

    Added gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp  = require("gulp");
+var sourcemaps = require("gulp-sourcemaps");
+var concat = require("gulp-concat");
+var sass  = require("gulp-sass");
+var babel = require("gulp-babel");
+
+var buildDir = "./build";
+
+gulp.task("styles", function() {
+	gulp.src("sass/**/*.scss")
+		.pipe(sass().on("error", sass.logError))
+		.pipe(gulp.dest(buildDir + "/css/"));
+});
+
+gulp.task("scripts", function() {
+	return gulp.src("src/**/*.js")
+		.pipe(sourcemaps.init())
+		.pipe(babel())
+		.pipe(concat("src.js"))
+		.pipe(sourcemaps.write("."))
+		.pipe(gulp.dest(buildDir + "/scripts/"));
+});
+
+gulp.task("default", function() {
+	gulp.watch("src/**/*.js", ["scripts"]);
+	gulp.watch("sass/**/*.scss", ["styles"]);
+});
