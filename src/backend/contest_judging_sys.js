cf1d5f5f58bd2fe7ab3e5a57dea4dbe5a3f9fa8c
Gigabyte-Giant <brynden.bielefeld@hotmail.com>
2015-09-25 18:36:06 -0500

    Updated src/backend/contest_judging_sys.js

---
+++ src/backend/contest_judging_sys.js
@@ -0,0 +1,14 @@
+import KA_API from "ka_api.js";
+import General from "../generalPurpose.js";
+
+var Contest_Judging_System = (function() {
+    if (!window.jQuery || !window.Firebase || !window.KA_API) {
+        return;
+    }
+
+    return {
+
+    };
+})();
+
+export default Contest_Judging_System;
