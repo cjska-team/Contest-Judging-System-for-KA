4c2150eb1374d61513cec802f409b59a17543f98
Gigabyte-Giant <brynden.bielefeld@hotmail.com>
2015-09-29 10:42:46 -0500

    Minor changes

--- src/client/index.js
+++ src/client/index.js
@@ -7,53 +7,65 @@
  * @todo (GigabyteGiant): Modify this function to use any data that gets passed in via the "contestData" object.
  */
 var createContestHolder = function(contestData) {
-    // Begin Column Root
-    var contestHolder = $("<div>")
-        .addClass("col").addClass("s12").addClass("m6")
+    // Begin Card Root
+    var contestHolder = $("<div>").addClass("card").addClass("contest")
+        // Begin Card Image
         .append(
-            // Begin Card Root
-            $("<div>").addClass("card")
-                // Begin Card Image
+            $("<div>").addClass("card-image").addClass("center")
                 .append(
-                    $("<div>").addClass("card-image")
-                        .append(
-                            $("<img>").attr("src", "http://newlitfromeurope.org/wp-content/uploads/2015/05/placeholder1.gif")
-                        )
-                        .append(
-                            $("<span>").addClass("card-title").addClass("amber").addClass("lighten-2")
-                                .text("Contest: Some contest")
-                        )
+                    $("<img>").attr("src", "http://newlitfromeurope.org/wp-content/uploads/2015/05/placeholder1.gif").css("width", "200px")
                 )
-                // End Card Image
-                // Begin Card Content
                 .append(
-                    $("<div>").addClass("card-content")
-                        .append(
-                            $("<p>").text("This is the description for some contest...")
-                        )
+                    $("<span>").addClass("card-title").addClass("amber").addClass("lighten-2")
+                        .text("Contest: Some contest")
                 )
-                // End Card Content
-                // Begin Card Action
-                .append(
-                    $("<div>").addClass("card-action").addClass("center")
-                        .append(
-                            $("<a>").attr("href", "javascript:void(0)").text("All Entries")
-                        )
-                        .append(
-                            $("<a>").attr("href", "javascript:void(0)").text("Leaderboard")
-                        )
-                )
-                // End Card Action
-            // End Card Root
         )
-    // End Column Root
+        // End Card Image
+        // Begin Card Content
+        .append(
+            $("<div>").addClass("card-content")
+                .append(
+                    $("<p>").text("This is the description for some contest...")
+                )
+        )
+        // End Card Content
+        // Begin Card Action
+        .append(
+            $("<div>").addClass("card-action").addClass("center")
+                .append(
+                    $("<a>").attr("href", "#").text("All Entries")
+                )
+                .append(
+                    $("<a>").attr("href", "#").text("Leaderboard")
+                )
+        );
+        // End Card Action
+    // End Card Root
 
     return contestHolder;
 };
 
+/**
+ * createColumn()
+ * Creates a materialize column div, and returns it to the caller.
+ * @author Gigabyte Giant (2015)
+ * @returns {jQuery} columnElement: The jQuery object containing the materialize column div.
+ */
+var createColumn = function() {
+    // Begin Column
+    var columnElement = $("<div>")
+        .addClass("col").addClass("s12").addClass("m12");
+    // End Column
+
+    return columnElement;
+};
+
 var setupPage = function() {
-    for (var i = 0; i < 7; i++) {
-        $(".row").append(createContestHolder({}));
+    for (var i = 0; i < 32; i++) {
+        $(".row").append(
+            createColumn()
+                .append(createContestHolder())
+        );
     }
 };
 
