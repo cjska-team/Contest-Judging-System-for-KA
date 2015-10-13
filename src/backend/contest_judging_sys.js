dcd488c50ee8754cd19c789da78553db73fc06bf
Gigabyte-Giant <brynden.bielefeld@hotmail.com>
2015-10-13 17:16:42 -0500

    Created a basic "fetchFirebaseAuth" function
    This function will declare a new instance of the Firebase object, and
    then invoke Firebase’s “getAuth” method, and return the result to the
    caller.

--- src/backend/contest_judging_sys.js
+++ src/backend/contest_judging_sys.js
@@ -1,9 +1,14 @@
 module.exports = (function() {
-    if (!window.jQuery || !window.Firebase || !window.KA_API) {
+    if (!window.jQuery || !window.Firebase) {
         return;
     }
 
-    return {
+    // Declare a new "constant" to hold our "Firebase Key".
+    let FIREBASE_KEY = "https://contest-judging-sys.firebaseio.com";
 
+    return {
+        fetchFirebaseAuth: function() {
+            return (new window.Firebase(FIREBASE_KEY)).getAuth();
+        }
     };
 })();
